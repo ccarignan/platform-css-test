53b702a360f7ba951f8c56d2f980ba89ceb1155d
Chris Carignan <chris.carignan@acquia.com>
2018-01-11 14:19:33 -0500

    initial commit

---
+++ src/js/aqkit.js
@@ -0,0 +1,6 @@
+import UIkit from '../../node_modules/uikit/src/js/uikit';
+import HelloWorld from './components/hello-world';
+
+UIkit.use(HelloWorld);
+
+export default UIkit;
---
+++ src/js/components/hello-world.js
@@ -0,0 +1,40 @@
+function plugin(UIkit) {
+
+  if (plugin.installed) {
+    return;
+  }
+
+  var {addClass, pointerDown} = UIkit.util;
+
+  UIkit.component('hello-world', {
+
+    init() {
+      console.log('init!', this);
+      addClass(this.$el, 'aq-hello-world');
+    },
+
+    methods: {
+
+      shout() {
+        alert('hello world');
+      }
+
+    },
+
+    events: {
+
+      [`${pointerDown}`](e) {
+        this.shout();
+      }
+
+    }
+
+  });
+
+}
+
+if (!BUNDLED && typeof window !== 'undefined' && window.UIkit) {
+  window.UIkit.use(plugin);
+}
+
+export default plugin;
