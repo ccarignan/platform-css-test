d0b8c3b90f2790c26b95fafee86eab2d53ba8142
Chris Carignan <chris.carignan@acquia.com>
2018-01-12 11:56:50 -0500

    merge custom-build branch

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # platform-css-test
+
+Setup:
+```
+npm install
+bundle install
+```
+
+To build dist files:
+```
+npm run compile
+```
+
+To serve docs site:
+```
+npm run docs
+```
+And go to [http://localhost:4000/docs](http://localhost:4000/docs)
---
+++ dist/js/components/hello-world.js
@@ -0,0 +1,50 @@
+/*! aqUIkit 0.0.0 | http://www.getuikit.com | (c) 2014 - 2017 YOOtheme | MIT License */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define('uikithello_world', factory) :
+	(global.aqUIkitHello_world = factory());
+}(this, (function () { 'use strict';
+
+function plugin(aqUIkit) {
+  var obj;
+
+
+  if (plugin.installed) {
+    return;
+  }
+
+  var ref = aqUIkit.util;
+  var addClass = ref.addClass;
+  var pointerDown = ref.pointerDown;
+
+  aqUIkit.component('hello-world', {
+
+    init: function init() {
+      console.log('init!', this);
+      addClass(this.$el, 'aq-hello-world');
+    },
+
+    methods: {
+
+      shout: function shout() {
+        alert('hello world');
+      }
+
+    },
+
+    events: ( obj = {}, obj[("" + pointerDown)] = function (e) {
+        this.shout();
+      }, obj)
+
+  });
+
+}
+
+if (!false && typeof window !== 'undefined' && window.aqUIkit) {
+  window.aqUIkit.use(plugin);
+}
+
+return plugin;
+
+})));
